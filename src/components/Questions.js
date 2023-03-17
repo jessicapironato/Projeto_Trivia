68f251818e89c422510c2248f643f8aeb4288a2e
AlineMSant <alinems4120@gmail.com>
2023-03-17 20:40:26 -0300

    requisito 6 - renderizando a categoria e a primeira questão

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,9 +4,17 @@ import { connect } from 'react-redux';
 import { fetchQuestions } from '../helpers/fetchHelpers';
 
 class Questions extends Component {
+  state = {
+    arrayQuestions: [],
+  };
+
   async componentDidMount() {
-    // const token = JSON.parse(localStorage.getItem('token'));
+    const token = JSON.parse(localStorage.getItem('token'));
     const resolvedFetch = await fetchQuestions(token);
+
+    this.setState({
+      arrayQuestions: resolvedFetch.results,
+    });
 
     if (resolvedFetch.response_code) {
       const { history } = this.props;
@@ -17,10 +25,18 @@ class Questions extends Component {
   }
 
   render() {
+    const { arrayQuestions } = this.state;
+    console.log(arrayQuestions);
+
     return (
       <div>
-        <h1 data-testid="question-category">category</h1>
-        <h1 data-testid="question-text">question</h1>
+        {arrayQuestions.length > 0 && (
+          <>
+            <h1 data-testid="question-category">{ arrayQuestions[0].category }</h1>
+            <h1 data-testid="question-text">{ arrayQuestions[0].question }</h1>
+          </>
+        )}
+
       </div>
     );
   }
