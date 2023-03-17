a59b7646c1c1987babdaef4b1764af5ed53f87cf
RaphaelMocellin <raphael.mocellin@gmail.com>
2023-03-17 16:15:00 -0300

    Elaborado e testado o req 1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import Game from './pages/Game';
-import Feedback from './pages/Feedback';
 
 function App() {
   return (
     <div>
-
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route path="/game" component={ Game } />
-        <Route path="/feedback" component={ Feedback } />
+        {/* <Route path="/game" component={ <Game /> } />
+        <Route path="/feedback" component={ <Feedback /> } /> */}
       </Switch>
 
     </div>
