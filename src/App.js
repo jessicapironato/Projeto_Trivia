4bf223c9c8d52a40f23bdd2a14dd2c739ce03b78
RaphaelMocellin <raphael.mocellin@gmail.com>
2023-03-19 13:18:49 -0300

    Elaborado e testado o req 11

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Settings from './components/Settings';
 import Game from './pages/Game';
+import Feedback from './pages/Feedback';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route exact path="/settings" component={ Settings } />
         <Route path="/game" component={ Game } />
-        {/* <Route path="/feedback" component={ <Feedback /> } /> */}
+        <Route path="/feedback" component={ Feedback } />
       </Switch>
 
     </div>
---
+++ src/pages/Feedback.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="feedback-text">Feedback</h1>
+      </div>
+    );
+  }
+}
+
+// Feedback.propTypes = {
+
+// };
+
+export default Feedback;
+
+// Requisito 11: Aline, Raphael, Carlos, Jéssica;
