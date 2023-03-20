eb321ad2f969417cf3f4d2fa506f201675b40ca8
Jéssica <jessicapmaximo@gmail.com>
2023-03-20 10:42:02 -0300

    requisito 12 elaborado e testado

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 // import PropTypes from 'prop-types';
 
 class Feedback extends Component {
@@ -6,6 +7,7 @@ class Feedback extends Component {
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
+        <Header />
       </div>
     );
   }
