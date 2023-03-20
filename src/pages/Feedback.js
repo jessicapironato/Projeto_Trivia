54d6433b4db0c88bff43b8c6040cdaf2ac1f1f82
RaphaelMocellin <raphael.mocellin@gmail.com>
2023-03-20 11:12:55 -0300

    Elaborado e testado o req 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
-// import PropTypes from 'prop-types';
 
 class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
+    const minAssertion = 3;
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
         <Header />
+        <h2
+          data-testid="feedback-text"
+        >
+          {assertions >= minAssertion ? 'Well Done!' : 'Could be better...'}
+
+        </h2>
       </div>
     );
   }
 }
 
-// Feedback.propTypes = {
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
 
-// };
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
 
-export default Feedback;
+export default connect(mapStateToProps)(Feedback);
 
 // Requisito 11: Aline, Raphael, Carlos, Jéssica;
+// Requisito 13: Raphael e Carlos
