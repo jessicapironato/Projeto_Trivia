b0c6326d686cdc4e89c1cdb1cf1003f9e1aacee1
Jéssica <jessicapmaximo@gmail.com>
2023-03-20 10:57:09 -0300

    requisito 14 parcialmente feito

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
+
 // import PropTypes from 'prop-types';
 
 class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
         <Header />
+        <h3
+          data-testid="feedback-total-question"
+        >
+          { assertions }
+        </h3>
       </div>
     );
   }
 }
 
-// Feedback.propTypes = {
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
 
-// };
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
 
-export default Feedback;
+export default connect(mapStateToProps)(Feedback);
 
 // Requisito 11: Aline, Raphael, Carlos, Jéssica;
