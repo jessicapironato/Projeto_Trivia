812b0b90f4e1d031bd7d5bf285c08f05bd2cf18f
Jéssica <jessicapmaximo@gmail.com>
2023-03-21 10:20:20 -0300

    realizado testes 100% ranking

--- src/tests/Ranking.test.js
+++ src/tests/Ranking.test.js
@@ -4,9 +4,21 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import { renderWithRouterAndRedux } from '../tests/helpers/renderWithRouterAndRedux'
 
-describe('', () => {
+describe('Testa componente Ranking', () => {
+    const route = '/ranking'
 
-  it('', ()=>{
-    
+
+  it('Tenta o redirecionamento para / após cliclar no botão', ()=>{
+
+    window.localStorage.setItem('ranking', JSON.stringify([{name: 'nome', email: 'test@test.com', score: 70}]))
+
+    const { history } =  renderWithRouterAndRedux(<App />, {}, route);
+
+    const buttonGoHome = screen.getByTestId('btn-go-home')
+       
+    expect(buttonGoHome).toBeVisible();
+    userEvent.click(buttonGoHome);
+
+    expect(history.location.pathname).toBe('/');
   })
 })
