f4b244bd03a41e54580b08d4e54bf1d5ef88ef3b
AlineMSant <alinems4120@gmail.com>
2023-03-22 16:54:37 -0300

    css feedback

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import md5 from 'crypto-js/md5';
 import PropTypes from 'prop-types';
 // import styles from '../styles/game.module.css';
 import '../styles/Game.css';
+import '../styles/Feedback.css';
 
 class Header extends Component {
   render() {
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import { resetScore } from '../redux/actions';
-import styles from '../styles/feedback.module.css';
+// import styles from '../styles/feedback.module.css';
+import '../styles/Feedback.css';
 
 class Feedback extends Component {
   handleClickPlayAgain = () => {
@@ -21,39 +22,60 @@ class Feedback extends Component {
     const { assertions, score } = this.props;
     const minAssertion = 3;
     return (
-      <div className={ styles.containerFeedback }>
-        <h1>Feedback</h1>
+      <div className="container-feedback-page">
         <Header />
-        <h2
-          data-testid="feedback-text"
-        >
-          {assertions >= minAssertion ? 'Well Done!' : 'Could be better...'}
+        <div className="container-out-feedback">
+          <div className="container-feedback">
+            <h1>Feedback</h1>
+            <h2
+              data-testid="feedback-text"
+            >
+              {assertions >= minAssertion ? 'Well Done!' : 'Could be better...'}
 
-        </h2>
-        <h3
-          data-testid="feedback-total-score"
-        >
-          { score }
-        </h3>
-        <h3
-          data-testid="feedback-total-question"
-        >
-          { assertions }
-        </h3>
-        <button
-          type="button"
-          data-testid="btn-play-again"
-          onClick={ this.handleClickPlayAgain }
-        >
-          Play Again
-        </button>
-        <button
-          type="button"
-          data-testid="btn-ranking"
-          onClick={ this.handleClickRanking }
-        >
-          Ranking
-        </button>
+            </h2>
+            <div className="feedback-assertions-score">
+              <h3 className="h3">
+                Voce acertou
+              </h3>
+              <h3
+                className="assertions"
+                data-testid="feedback-total-question"
+              >
+                { assertions }
+              </h3>
+              <h3 className="h3">questões!</h3>
+            </div>
+            <div className="feedback-assertions-score">
+              <h3 className="h3">Um total de</h3>
+              <h3
+                className="score"
+                data-testid="feedback-total-score"
+              >
+                { score }
+              </h3>
+
+              <h3 className="h3">pontos</h3>
+            </div>
+          </div>
+          <div className="btn-feedback">
+            <button
+              className="btn-play-again"
+              type="button"
+              data-testid="btn-play-again"
+              onClick={ this.handleClickPlayAgain }
+            >
+              Play Again
+            </button>
+            <button
+              className="btn-ranking"
+              type="button"
+              data-testid="btn-ranking"
+              onClick={ this.handleClickRanking }
+            >
+              Ranking
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
